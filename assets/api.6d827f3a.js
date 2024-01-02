e6b9aa0ad7668ff5c40d4de132ee167f0d963868
Tobias Mesquita <tobias.mesquita@gmail.com>
2024-01-02 09:54:23 -0300

    dist pwa

---
+++ assets/api.6d827f3a.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index.eccdee0a.js";import{c as r}from"./api.f87e1490.js";var e=o(({store:a})=>{r(a)});export{e as default};
---
+++ assets/prefetch.a6a38b65.js
@@ -0,0 +1,1 @@
+import{i as t}from"./index.eccdee0a.js";const n=Symbol("is-fetch-enabled-key");function s(){return t(n)}async function i(e){if(s().value)return await e()}export{n as i,i as u};
